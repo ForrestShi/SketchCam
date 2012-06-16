b51f86969cdfac9bf7aa9032b2a54bf3635fdd12
Forrest <apple.dev.sh@gmail.com>
2012-06-16 13:26:34 +0800

    Initial Commit

---
+++ SketchCam/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SketchCam
+//
+//  Created by Shi Forrest on 12-6-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SketchCam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SketchCam
+//
+//  Created by Shi Forrest on 12-6-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
