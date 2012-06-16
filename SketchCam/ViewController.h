713dd8026a3df2575c2b1fc0627253af4b84b879
Forrest <apple.dev.sh@gmail.com>
2012-06-16 14:19:14 +0800

    this is a complete basic infrastructure for other apps starting

--- SketchCam/ViewController.h
+++ SketchCam/ViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "GPUImage.h"
 
 @interface ViewController : UIViewController
 
--- SketchCam/ViewController.m
+++ SketchCam/ViewController.m
@@ -7,17 +7,79 @@
 //
 
 #import "ViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
 
-@interface ViewController ()
+
+@interface ViewController (){
+    GPUImageStillCamera *stillCamera;
+    GPUImageOutput<GPUImageInput> *filter;
+    UISlider *filterSettingsSlider;
+    UIButton *photoCaptureButton;
+}
+
+- (void)updateSliderValue:(id)sender;
+- (void)takePhoto:(id)sender;
 
 @end
 
 @implementation ViewController
 
+
+- (void)loadView 
+{
+	CGRect mainScreenFrame = [[UIScreen mainScreen] bounds];
+    
+    // Yes, I know I'm a caveman for doing all this by hand
+	GPUImageView *primaryView = [[GPUImageView alloc] initWithFrame:mainScreenFrame];
+	primaryView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    
+    filterSettingsSlider = [[UISlider alloc] initWithFrame:CGRectMake(25.0, mainScreenFrame.size.height - 50.0, mainScreenFrame.size.width - 50.0, 40.0)];
+    [filterSettingsSlider addTarget:self action:@selector(updateSliderValue:) forControlEvents:UIControlEventValueChanged];
+	filterSettingsSlider.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
+    filterSettingsSlider.minimumValue = 0.0;
+    filterSettingsSlider.maximumValue = 3.0;
+    filterSettingsSlider.value = 1.0;
+    
+    [primaryView addSubview:filterSettingsSlider];
+    
+    photoCaptureButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    photoCaptureButton.frame = CGRectMake(round(mainScreenFrame.size.width / 2.0 - 150.0 / 2.0), mainScreenFrame.size.height - 90.0, 150.0, 40.0);
+    [photoCaptureButton setTitle:@"Capture Photo" forState:UIControlStateNormal];
+	photoCaptureButton.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
+    [photoCaptureButton addTarget:self action:@selector(takePhoto:) forControlEvents:UIControlEventTouchUpInside];
+    [photoCaptureButton setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
+    
+    [primaryView addSubview:photoCaptureButton];
+    
+	self.view = primaryView;	
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    stillCamera = [[GPUImageStillCamera alloc] init];
+    //    stillCamera = [[GPUImageStillCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+    stillCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    //    filter = [[GPUImageGammaFilter alloc] init];
+    filter = [[GPUImageSketchFilter alloc] init];
+    //    filter = [[GPUImageSmoothToonFilter alloc] init];
+    //    filter = [[GPUImageSepiaFilter alloc] init];
+    
+    
+	[filter prepareForImageCapture];
+    
+    [stillCamera addTarget:filter];
+    GPUImageView *filterView = (GPUImageView *)self.view;
+    [filter addTarget:filterView];
+    
+    //    [stillCamera.inputCamera lockForConfiguration:nil];
+    //    [stillCamera.inputCamera setFlashMode:AVCaptureFlashModeOn];
+    //    [stillCamera.inputCamera unlockForConfiguration];
+    
+    [stillCamera startCameraCapture];
+
 }
 
 - (void)viewDidUnload
@@ -34,5 +96,42 @@
         return YES;
     }
 }
+
+- (void)updateSliderValue:(id)sender
+{
+    float value = [(UISlider*)sender value] ;
+    [(GPUImageSketchFilter *)filter setTexelHeight:(value / 480.0)];
+    [(GPUImageSketchFilter *)filter setTexelWidth:(value / 360.0)];
+    
+}
+
+- (void)takePhoto:(id)sender;
+{
+    [photoCaptureButton setEnabled:NO];
+    
+    [stillCamera capturePhotoAsJPEGProcessedUpToFilter:filter withCompletionHandler:^(NSData *processedJPEG, NSError *error){
+        
+        // Save to assets library
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        //        report_memory(@"After asset library creation");
+        
+        [library writeImageDataToSavedPhotosAlbum:processedJPEG metadata:nil completionBlock:^(NSURL *assetURL, NSError *error2)
+         {
+             //             report_memory(@"After writing to library");
+             if (error2) {
+                 NSLog(@"ERROR: the image failed to be written");
+             }
+             else {
+                 NSLog(@"PHOTO SAVED - assetURL: %@", assetURL);
+             }
+			 
+             runOnMainQueueWithoutDeadlocking(^{
+                 //                 report_memory(@"Operation completed");
+                 [photoCaptureButton setEnabled:YES];
+             });
+         }];
+    }];
+}
+
 
 @end
